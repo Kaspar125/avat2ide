f48b49f732aafbb8e36a7d833a0028f562a4f129
mait.kaasik <mait.kaasik@helmes.com>
2024-06-01 22:45:50 +0300

    hinnaparing wip

--- app/components/aken.tsx
+++ app/components/aken.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {WindowTypes} from '@/components/types';
 import {Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter} from '@/components/ui/card';
+import {Button} from '@/components/ui/button';
 
 export default function WindowTypesList() {
     const [selectedWindowType, setSelectedWindowType] = useState("");
@@ -12,25 +13,28 @@ export default function WindowTypesList() {
     return (
         <div className="grid grid-cols-3 gap-4 my-6">
             {WindowTypes.map((windowType) => (
-                <div
-                    key={windowType.code}
-                    onClick={() => handleSelect(windowType.code)}
-                >
-                    <Card className="w-[350px]">
-                        <CardHeader>
-                            <CardTitle></CardTitle>
-                        </CardHeader>
-                        <CardContent className={"justify-center flex"}>
-                            <img src={"/aknamudelid/" + windowType.img} alt={windowType.name} />
-                        </CardContent>
-                        <CardFooter className="justify-center">
-                            {windowType.name}
-                        </CardFooter>
-                    </Card>
-                </div>
+                (selectedWindowType === '' || windowType.code === selectedWindowType) && (
+                    <div
+                        key={windowType.code}
+                        onClick={() => handleSelect(windowType.code)}
+                    >
+                        <Card className="w-[350px]">
+                            <CardHeader>
+                                <CardTitle>{windowType.name}</CardTitle>
+                                <CardDescription></CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                            </CardContent>
+                            <CardFooter className="flex justify-between">
+                                {windowType.code}
+                            </CardFooter>
+                        </Card>
+                    </div>
+                )
             ))}
             <input type="hidden" value={selectedWindowType} />
             <p>Selected Window Type: {selectedWindowType}</p>
+            <Button onClick={() => setSelectedWindowType("")}>Clear</Button>
         </div>
     );
 }
