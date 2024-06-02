7147ff3216c55b95a5c4b6bf4fb4303af8a51c5b
kaspar125 <kaasik.kaspar22@gmail.com>
2024-06-02 18:58:15 +0300

    contact migration

---
+++ prisma/migrations/20240602155719_contact/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `File` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Issue` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "File";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Issue";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
